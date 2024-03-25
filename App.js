734bc58ee48d58b6cc85933d32b9c3daa3d3361b
Vanh2k20 <avan81837@gmail.com>
2024-03-25 17:07:21 +0700

    vanh code

--- App.js
+++ App.js
@@ -2,25 +2,31 @@ import { useEffect, useState } from 'react';
 import WelcomeScreen from './screens/WelcomeScreen';
 import AuthNavigator from './navigator/AuthNavigator';
 import { NavigationContainer } from '@react-navigation/native';
+import { StatusBar } from 'react-native';
+import ManageUser from './screens/ManageUser';
+import KhachHangScreen from './screens/KhachHangScreen';
 
 export default function App() {
-  const [checkWelcome, setcheckWelcome] = useState(true);
+  // const [checkWelcome, setcheckWelcome] = useState(true);
 
-  useEffect(() => {
-    const timeout = setTimeout(() => {
-      setcheckWelcome(false);
-    }, 2000);
-    return () => clearTimeout(timeout)
-  }, [])
+  // useEffect(() => {
+  //   const timeout = setTimeout(() => {
+  //     setcheckWelcome(false);
+  //   }, 2000);
+  //   return () => clearTimeout(timeout)
+  // }, [])
 
-  return (
-    checkWelcome
-      ? <WelcomeScreen />
-      : <NavigationContainer>
-        <AuthNavigator />
-      </NavigationContainer>
-    // <NavigationContainer>
-    //   <MainNavigator/>
-    // </NavigationContainer>
-  );
+  // return (
+  //   // checkWelcome
+  //   //   ? <WelcomeScreen />
+  //   //   : <NavigationContainer>
+  //   //     <AuthNavigator />
+  //   //   </NavigationContainer>
+  //   <NavigationContainer>
+  //     <MainNavigator/>
+  //   </NavigationContainer>
+  // );
+  return(
+    <KhachHangScreen/>
+  )
 }
---
+++ screens/DichVuChiTiet.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const DichVuChiTiet = () => {
+  return (
+    <View>
+      <Text>DichVuChiTiet</Text>
+    </View>
+  )
+}
+
+export default DichVuChiTiet
+
+const styles = StyleSheet.create({})
