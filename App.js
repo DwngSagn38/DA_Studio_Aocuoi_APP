ba87f4e5cdb2de8b42c63ef1a60f522fdd8f42f3
Vanh2k20 <avan81837@gmail.com>
2024-03-25 11:36:08 +0700

    AuthNavigator

--- App.js
+++ App.js
@@ -1,20 +1,26 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useEffect, useState } from 'react';
+import WelcomeScreen from './screens/WelcomeScreen';
+import AuthNavigator from './navigator/AuthNavigator';
+import { NavigationContainer } from '@react-navigation/native';
 
 export default function App() {
+  const [checkWelcome, setcheckWelcome] = useState(true);
+
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setcheckWelcome(false);
+    }, 2000);
+    return () => clearTimeout(timeout)
+  }, [])
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    checkWelcome
+      ? <WelcomeScreen />
+      : <NavigationContainer>
+        <AuthNavigator />
+      </NavigationContainer>
+    // <NavigationContainer>
+    //   <MainNavigator/>
+    // </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ screens/HoaDonScreen.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const HoaDonScreen = () => {
+  return (
+    <View>
+      <Text>HoaDonScreen</Text>
+    </View>
+  )
+}
+
+export default HoaDonScreen
+
+const styles = StyleSheet.create({})
