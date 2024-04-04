f32010611a3ded982df57a4ad8be19718c86ba9a
Vanh2k20 <avan81837@gmail.com>
2024-04-04 12:25:22 +0700

    vanh

--- screens/Profile.js
+++ screens/Profile.js
@@ -1,9 +1,13 @@
-import { Image, StyleSheet, Text, TouchableOpacity, View } from 'react-native'
+import { Image, Modal, Pressable, StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native'
 import React, { useEffect, useState } from 'react'
 import AsyncStorage from '@react-native-async-storage/async-storage'
 
 const Profile = ({ navigation }) => {
   const [User, setUser] = useState([]);
+  const [modalVisible, setModalVisible] = useState(false);
+  const [passOld, setPassOld] = useState('');
+  const [passNew, setPassNew] = useState('');
+  const [checkPass, setCheckPass] = useState('');
 
   // lấy user từ AsyncStorage
   const retrieveData = async () => {
@@ -47,16 +51,124 @@ const Profile = ({ navigation }) => {
       <View style={styles.option}>
         <Text style={styles.textGray}>Chung
           {'\n'}_________________________________________________</Text>
-        <Text onPress={() => navigation.navigate('ManageUser',{User : User})}>Chỉnh sửa thông tin</Text>
+        <Text onPress={() => navigation.navigate('ManageUser', { User: User })}>Chỉnh sửa thông tin</Text>
         <Text>Chi tiết công việc</Text>
       </View>
 
       <View style={styles.option}>
         <Text style={styles.textGray}>Bảo mật và điều khoản
           {'\n'}_________________________________________________</Text>
-        <Text>Quên mật khẩu</Text>
-        <Text>Đổi mật khẩu</Text>
+
+        <TouchableOpacity onPress={() => setModalVisible(true)}>
+          <Text>
+            Đổi mật khẩu
+          </Text>
+        </TouchableOpacity>
       </View>
+
+      <Modal
+        style={{
+          height: "100%",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+        animationType="slide"
+        transparent={true}
+        visible={modalVisible}
+        onRequestClose={() => {
+          setModalVisible(!modalVisible);
+        }}
+      >
+        <View
+          style={{
+            height: "100%",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          <View
+            style={{
+              width: "90%",
+              margin: 20,
+              backgroundColor: "white",
+              borderRadius: 20,
+              padding: 35,
+              alignItems: "center",
+              shadowColor: "#000",
+              shadowOffset: { width: 0, height: 2 },
+              shadowOpacity: 0.25,
+              shadowRadius: 4,
+              elevation: 5,
+            }}
+          >
+            <Text
+              style={{
+                fontSize: 18,
+                fontWeight: "bold",
+                textAlign: "center",
+                marginBottom: 10,
+              }}
+            >
+              Thêm nhân viên mới
+            </Text>
+
+            <View style={styles.khachHang}>
+              <View style={{ gap: 5, marginTop: 10 }}>
+                <TextInput
+                  style={{
+                    padding: 10,
+                    borderRadius: 10,
+                    borderWidth: 1,
+                    width: "100%",
+                  }}
+                  value={passOld}
+                  onChangeText={(txt) => setPassOld(txt)}
+                  placeholder="Mật khẩu cũ"
+                />
+                <TextInput
+                  value={passNew}
+                  onChangeText={(txt) => setPassNew(txt)}
+                  style={{
+                    padding: 10,
+                    borderRadius: 10,
+                    borderWidth: 1,
+                    width: "100%",
+                  }}
+                  placeholder="Mật khẩu mới"
+                />
+                <TextInput
+                  value={checkPass}
+                  onChangeText={(txt) => setCheckPass(txt)}
+                  style={{
+                    padding: 10,
+                    borderRadius: 10,
+                    borderWidth: 1,
+                    width: "100%",
+                  }}
+                  placeholder="Nhập lại mật khẩu mới"
+                />
+              </View>
+            </View>
+            <View style={{ flexDirection: "row" }}>
+              <Pressable
+                style={[styles.button, styles.buttonClose]}
+                onPress={() => {
+                  setModalVisible(!modalVisible),
+                    resetData()
+                }}
+              >
+                <Text style={styles.textStyle}>Cancel</Text>
+              </Pressable>
+              <Pressable
+                style={[styles.button, styles.buttonClose]}
+                onPress={() => { saveNV() }}
+              >
+                <Text style={styles.textStyle}>Save</Text>
+              </Pressable>
+            </View>
+          </View>
+        </View>
+      </Modal>
     </View>
   )
 }
